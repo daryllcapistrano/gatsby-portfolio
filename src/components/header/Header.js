5d5128d2ec8921ca38f989f8a3fa71348490ef33
Daryll <dcapistrano.dev@gmail.com>
2020-07-27 17:16:29 -0700

    minor header style fixes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,17 +1,25 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
 import Container from "@material-ui/core/Container"
 import useScrollTrigger from "@material-ui/core/useScrollTrigger"
 import Slide from "@material-ui/core/Slide"
+import Button from "@material-ui/core/Button"
+
+const NavContainer = styled.ul`
+  display: inline-flex;
+  list-style-type: none;
+  padding: 0;
+`
 
 function HideOnScroll(props) {
   const { children } = props
   const trigger = useScrollTrigger()
 
   return (
-    <Slide appear={false} direction="down" in={!trigger}>
+    <Slide appear={false} direction="down" in={!trigger} timeout={400}>
       {children}
     </Slide>
   )
@@ -30,18 +38,26 @@ export default function Header(props) {
 
   return (
     <HideOnScroll {...props}>
-      <AppBar position="fixed" color="transparent">
+      <AppBar position="fixed" color="transparent" elevation={0}>
         <Toolbar>
           <Container>{data.site.siteMetadata.title}</Container>
-          <Container>
-            <p>projects</p>
-          </Container>
-          <Container>
-            <p>about me</p>
-          </Container>
-          <Container>
-            <p>contact</p>
-          </Container>
+          <NavContainer>
+            <li>
+              <Button color="inherit" variant="text" size="medium">
+                projects
+              </Button>
+            </li>
+            <li>
+              <Button color="inherit" variant="text" size="medium">
+                about
+              </Button>
+            </li>
+            <li>
+              <Button color="inherit" variant="text" size="medium">
+                contacts
+              </Button>
+            </li>
+          </NavContainer>
         </Toolbar>
       </AppBar>
     </HideOnScroll>
